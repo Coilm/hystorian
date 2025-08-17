ac21d8c5b23c9b2f0ff769bfcc5dbea5bdcbe58b
Loïc Musy <loic@famille-musy.com>
2025-08-17 17:13:44 +0200

    Add import to __init__ in io folder to streamline HyFile and HyPath imports.

---
+++ hystorian/io/__init__.py
@@ -0,0 +1,5 @@
+
+from .HyExtractor import add_converter, extract, remove_converter
+from .hyFile import HyFile, HyPath
+
+__all__ = ['HyFile', 'HyPath', 'add_converter', 'remove_converter', 'extract']
