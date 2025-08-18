ccbff3d5832cdcd01ec09765c13a871df16d3f7c
Loïc Musy <loic@famille-musy.com>
2025-08-18 13:25:43 +0200

    update test to be up-to-date with new import

--- tests/test_io.py
+++ tests/test_io.py
@@ -4,9 +4,8 @@ import unittest
 
 import numpy as np
 import pytest
-from igor2 import binarywave
 
-from hystorian.io import hyFile, utils
+from hystorian.io import HyFile, utils
 
 filepath = pathlib.Path("tests/test_files/test.hdf5")
 
@@ -17,38 +16,38 @@ def teardown_module():
         os.remove(filepath)
 
 
-class TestHyFileClass:
+class TestHyFileClass(unittest.TestCase):
     def test_init(self):
-        with hyFile.HyFile(filepath) as f:
+        with HyFile(filepath) as f:
             root_keys = list(f[""])
 
         assert root_keys == ["datasets", "metadata", "process"]
 
     def test_double_init(self):
-        with hyFile.HyFile(filepath) as f:
+        with HyFile(filepath) as f:
             pass
 
-        with hyFile.HyFile(filepath) as f:
+        with HyFile(filepath) as f:
             root_keys = list(f[""])
 
         assert root_keys == ["datasets", "metadata", "process"]
 
     def test_write(self):
-        with hyFile.HyFile(filepath, "r+") as f:
+        with HyFile(filepath, "r+") as f:
             f._create_dataset(("datasets/test_data", np.arange(100)))
             data = f.read("datasets/test_data")
 
         assert (data == np.arange(100)).all()
 
     def test_deep_write(self):
-        with hyFile.HyFile(filepath, "r+") as f:
+        with HyFile(filepath, "r+") as f:
             f._create_dataset(("datasets/a/b/test_data", np.arange(100)))
             data = f.read("datasets/a/b/test_data")
 
         assert (data == np.arange(100)).all()
 
     def test_overwrite(self):
-        with hyFile.HyFile(filepath, "r+") as f:
+        with HyFile(filepath, "r+") as f:
             f._create_dataset(("datasets/test_data", np.arange(100)))
             f._create_dataset(("datasets/test_data", np.arange(200)))
 
@@ -60,7 +59,7 @@ class TestHyFileClass:
             f._create_dataset(("datasets/test_data", np.arange(200)), overwrite=False)
 
     def test_deletion(self):
-        with hyFile.HyFile(filepath, "r+") as f:
+        with HyFile(filepath, "r+") as f:
             f._create_dataset(("datasets/test_data", np.arange(100)))
             del f["datasets/test_data"]
 
@@ -68,13 +67,13 @@ class TestHyFileClass:
                 f["datasets/test_data"]
 
     def test_contain(self):
-        with hyFile.HyFile(filepath) as f:
+        with HyFile(filepath) as f:
             assert "datasets" in f
 
             assert "not_a_folder" not in f
 
 
-class TestHyFileWrite:
+class TestHyFileWrite(unittest.TestCase):
     def test_write_extracted_data(self):
         fake_path = "tmp_file"
         data = {"a": np.arange(100), "b": np.arange(200)}
@@ -82,14 +81,13 @@ class TestHyFileWrite:
         metadata = {"a": "This is metadata"}
 
         extracted = utils.HyConvertedData(data, metadata, attr)
-        with hyFile.HyFile(filepath, "r+") as f:
+        with HyFile(filepath, "r+") as f:
             f._write_extracted_data(pathlib.Path(fake_path), extracted)
 
             assert f[f"datasets/{fake_path}/a"].attrs["name"] == "test_a"
             assert f[f"datasets/{fake_path}/b"].attrs["name"] == "test_b"
             assert f[f"datasets/{fake_path}/b"].attrs["size"] == 200
             assert (f[f"datasets/{fake_path}/b"] == np.arange(200)).all()
-
 
 if __name__ == "__main__":
     unittest.main()
